25b62a3af2c8ca77f59617371f71a5d6a3dbc6f9
Geneus003 <Geneus003@users.noreply.github.com>
2018-11-12 19:22:22 +0300

    Nee to check work

--- Mountains.java
+++ Mountains.java
@@ -13,27 +13,105 @@ public class Main{
 
         System.out.println("Введите ширину");
         int wht = vvod.nextInt();
-        int moun[] = new int[wht];
+        int matrix[] = new int[wht];
 
         for (int i = 0; i < wht; i++){
             System.out.println("Высота рельефа на участке " + i);
-            moun[i] = vvod.nextInt();
+            matrix[i] = vvod.nextInt();
         }
 
-        int end_con = 0;
+        int passed = -1, zerno = -1;
+        int min_l = -1, cord1 = -1;
+        int min_r = -1, cord2 = -1;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < wht; j++) {
+
+                if (j == passed){
+                    continue;
+                }
+                if (matrix[j] > min_l){
+                    min_l = matrix[j];
+                    cord1 = j;
+                    zerno = j;
+                    continue;
+                }
+                if ((matrix[j] >= min_r) && (passed != -1)){
+                    cord2 = j;
+                    min_r = matrix[j];
+                }
+
+            }
+            passed = zerno;
+        }
+
+        int s = 0;
+        for(int i = cord1 + 1; i < cord2; i++){
+            s += Math.min(cord1, cord2);
+        }
+
+        s += find_to_towns_l(cord1, 0, matrix, wht);
+        System.out.println("______________");
+        s += find_to_towns_r(wht - 1, cord2, matrix, wht-1);
+
+        System.out.println(s);
+    }
+
+    public static int find_to_towns_l(int cord_min_r, int cord_min_l, int[] matrix, int wht){
+
+        wht -= 1;
+
+        if ((cord_min_r == 0) && (cord_min_l == 0)){
+            return 0;
+        }
+
+        int max_v = -1;
+        int cor = -1;
         int con = 0;
-        int maxx = moun[0];
-        for (int i = 1; i < wht; i ++){
-            if (moun[i] >= maxx){
-                maxx = moun[i];
-                end_con += con;
-                con = 0;
-            }
-            else{
-                con += maxx - moun[i];
-            }
+
+        for(int i = cord_min_l; i < cord_min_r; i++){
+            if (matrix[i] > max_v)
+                max_v = matrix[i];
+                cor = i;
         }
 
-        System.out.println(end_con);
+        for(int i = cor; i < cord_min_r; i++){
+            if (max_v - matrix[i] < 0){
+                continue;
+            }
+            con += max_v - matrix[i];
+        }
+
+        con += (find_to_towns_l(cor, 0, matrix, wht));
+
+        return con;
+    }
+
+    public static int find_to_towns_r(int cord_min_r, int cord_min_l, int[] matrix, int wht){
+
+        if (((cord_min_r == wht) && (cord_min_l == wht)) ){
+            return 0;
+        }
+
+        int max_v = -1;
+        int cor = -1;
+        int con = 0;
+
+        for(int i = cord_min_l + 1; i <= cord_min_r; i++){
+            if (matrix[i] > max_v)
+                max_v = matrix[i];
+                cor = i;
+        }
+
+        for(int i = cord_min_l; i < cor; i++) {
+            if (max_v - matrix[i] < 0) {
+                continue;
+            }
+            con += max_v - matrix[i];
+        }
+
+        con += (find_to_towns_r(wht, cor, matrix, wht));
+
+        return con;
     }
 }
